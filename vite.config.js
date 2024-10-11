09034da74099da2950dc23812933850f92d8736b
Sahil Amin <sahilamin68@gmail.com>
2024-10-11 13:19:36 +0530

    vite.config modified

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,15 @@ export default {
     publicDir: '../public/',    // Updated public directory, where static assets like index.html and favicon.ico are placed
     base: './',                 // Ensures relative paths for assets, helpful when deploying to subdirectories
     build: {
+      sourcemap: true,
       outDir: '../dist',        // Output directory for build files, placed outside 'src'
       emptyOutDir: true,        // Cleans the output directory before building
       rollupOptions: {
-        input: '/index.html',  // Set entry point as 'public/index.html'
+        main: path.resolve(__dirname, 'src/index.html'),
+        // List your main component files here
+        Airplane: path.resolve(__dirname, 'src/components/Airplane.js'),
+        Pilot: path.resolve(__dirname, 'src/components/Pilot.js'),
+        Sea: path.resolve(__dirname, 'src/components/Sea.js'),
       },
     },
     server: {
@@ -17,9 +22,9 @@ export default {
     },
     resolve: {
       alias: {
-        '/@assets': path.resolve(__dirname, './src/assets'),       // Correct alias for assets directory
-        '/@components': path.resolve(__dirname, './src/components'),  // Correct alias for components directory
-        '/@utils': path.resolve(__dirname, './src/utils'), 
+        '@assets': path.resolve(__dirname, './src/assets'),       // Correct alias for assets directory
+        '@components': path.resolve(__dirname, './src/components'),  // Correct alias for components directory
+        '@utils': path.resolve(__dirname, './src/utils'), 
       },
     },
 }
